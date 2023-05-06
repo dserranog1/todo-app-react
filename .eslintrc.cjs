f4c3424b2e4a8c492f4fae161e2823faed7d23ac
Daniel Serrano <dserranog@unal.edu.co>
2023-05-06 15:48:05 -0500

    add todo list

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,30 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:import/recommended',
-    'plugin:jsx-a11y/recommended',
-    'eslint-config-prettier',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "eslint-config-prettier",
   ],
-   settings: {
+  settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
     // Tells eslint how to resolve imports
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-     "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": "warn",
+    "react/react-in-jsx-scope": "off",
   },
-}
-
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../public/vite.svg'
-import './App.css'
+import TodoComponent from "./components/TodoComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full flex flex-col justify-center items-center my-auto h-full min-h-screen bg-gray-200 text-2xl py-6 text-gray-600">
+      <div className="flex flex-col items-center gap-4">
+        <h1>Todo App</h1>
+        <h2 className="text-lg">Click any todo to edit it (when unchecked)</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <TodoComponent />
+    </div>
+  );
 }
 
-export default App
+export default App;
